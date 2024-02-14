a3f7c3ae74403aeae194c6010547a5bb706968ca
Jim Weller <jim.weller@gmail.com>
2024-02-13 21:38:57 -0800

    workflow Tue Feb 13 21:38:57 PST 2024

--- ssm_param.tf
+++ ssm_param.tf
@@ -30,7 +30,7 @@ variable "environment_type" {
 }
 
 resource "aws_ssm_parameter" "environment_type" {
-  name        = "environment_type26"
+  name        = "environment_type27"
   type        = "String"
   value       = var.environment_type
   description = "basic string  to see what terraform & GH are doing"
