b284a2e4bcae9a8743434c2018a41fbe767b030e
Jim Weller <jim.weller@gmail.com>
2024-02-13 23:27:14 -0800

    it

--- ssm_param.tf
+++ ssm_param.tf
@@ -29,8 +29,8 @@ provider "aws" {
 #  default = "nothing"
 #}
 
-resource "aws_ssm_parameter" "environment_type1" {
-  name        = "environment_type"
+resource "aws_ssm_parameter" "environment_type2" {
+  name        = "environment_type2"
   type        = "String"
   value       = "13" #var.environment_type
   description = "basic string  to see what terraform & GH are doing"
@@ -41,15 +41,15 @@ resource "aws_ssm_parameter" "environment_type1" {
     Owner                = "CPEENBL"
     Platform             = "AWS Delivery"
     Product              = "OnBase"
-    git_commit           = "62db65792452d6b9c935b1d2a9577554bec5744c"
+    git_commit           = "956f86fa9b26ceaf186fa05b19c12cb718339bf0"
     git_file             = "ssm_param.tf"
-    git_last_modified_at = "2024-02-14 07:17:35"
+    git_last_modified_at = "2024-02-14 07:21:07"
     git_last_modified_by = "jim.weller@gmail.com"
     git_modifiers        = "jim.weller"
     git_org              = "jimweller"
     git_repo             = "tf-envs"
     source               = "yor"
-    yor_name             = "environment_type"
+    yor_name             = "environment_type1"
     yor_trace            = "9480d18a-6909-4291-aeff-2430996b85fe"
   }
 }
